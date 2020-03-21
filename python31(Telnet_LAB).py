de46842264eabe98e5bc1abf7b4f4722138c5b10
celestine <nwabuisicelestine@gmail.com>
2020-03-21 14:39:28 +0100

    modified:   python31(Telnet_LAB).py

--- python31(Telnet_LAB).py
+++ python31(Telnet_LAB).py
@@ -1,22 +1,30 @@
 import getpass
-import telnetlib    #importing the telnet library 
+import telnetlib  # importing the telnet library
 
-HOST = "192.168.122.254" 
+HOST = "192.168.122.254"
 user = input("Enter your remote telnet username: ")
 
 password = getpass.getpass()  # requesting for a password
 
 if password == 'cisco':
-   print('Connecting to ' + HOST)
-   Tel = telnetlib.Telnet(HOST)  # telneting to the switch
+   print('Connecting to R1 @ ' + HOST)
+   try:
+       Tel = telnetlib.Telnet(HOST)  # telneting to the switch
+   except:
+       print('Unable to establish remote connection... transport input maybe set to none')
+       exit()
 elif password != 'cisco':
    print('Please input correct password....')
+   exit()
 
-Tel.read_until(b"Username: ")  #runs the code until Username is displayed 
-Tel.write(user.encode('ascii') + b"\n") #write the user.variable to the network device
+
+Tel.read_until(b"Username: ")  # runs the code until Username is displayed
+# write the user.variable to the network device
+Tel.write(user.encode('ascii') + b"\n")
 if password:
-    Tel.read_until(b"Password: ") #runs the code until Password is displayed 
-    Tel.write(password.encode('ascii') + b"\n")  #write the password.variable to the network device
+    Tel.read_until(b"Password: ")  # runs the code until Password is displayed
+    # write the password.variable to the network device
+    Tel.write(password.encode('ascii') + b"\n")
 
 Tel.write(b"enable\n")
 Tel.write(b"cisco\n")
@@ -33,4 +41,4 @@ Tel.write(b"end\n")
 Tel.write(b"wr\n")
 Tel.write(b"exit\n")
 
-print(Tel.read_all().decode('ascii')) 
+print(Tel.read_all().decode('ascii'))
